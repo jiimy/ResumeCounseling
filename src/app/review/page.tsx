9e83de7046ff3d7e8d940a6383f3474ba91e5bda
rudwnok <rudwnok123@naver.com>
2025-04-08 01:53:47 +0900

    전체 리뷰 페이지 수정

--- src/app/review/page.tsx
+++ src/app/review/page.tsx
@@ -3,11 +3,11 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const ReviewPage = () => {
-  const { data, isLoading, isSuccess } = useQuery({
-      queryFn: () => reviewAlltApi(0, 10),
-      queryKey: ['reviewList'],
-      staleTime: 1000 * 60 * 5,
-    });
+  // const { data, isLoading, isSuccess } = useQuery({
+  //     queryFn: () => reviewAlltApi(0, 10),
+  //     queryKey: ['reviewList'],
+  //     staleTime: 1000 * 60 * 5,
+  //   });
 
   return (
     <div>
