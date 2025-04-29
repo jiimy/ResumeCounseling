4c8b32e6060891c41e03bd97bac8435c5f649804
rudwnok <rudwnok123@naver.com>
2025-04-29 14:58:56 +0900

    테스트

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,48 +1,21 @@
-// "use client";
-
-// import { useEffect } from "react";
-// import { useRouter } from "next/navigation";
-// import { createClient } from "@/util/supabase/client";
-// import { UserStore } from "@/store/user";
-
-// export default function AuthCallback() {
-//   const supabase = createClient();
-//   const router = useRouter();
-//   const setUser = UserStore((state) => state.setName);
-
-//   useEffect(() => {
-//     const checkSession = async () => {
-//       const { data, error } = await supabase.auth.getSession();
-//       console.log('data', data);
-//       setUser(data.session?.user?.user_metadata?.full_name);
-
-//       if (error || !data.session) {
-//         console.error("Authentication failed:", error);
-//         router.push("/login");
-//       } else {
-//         router.push("/"); // 로그인 후 이동할 페이지
-//       }
-//     };
-
-//     checkSession();
-//   }, [router]);
-
-//   return <p>로그인 중...</p>;
-// }
 "use client";
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { createClient } from "@/util/supabase/client";
+import { UserStore } from "@/store/user";
 
 export default function AuthCallback() {
   const supabase = createClient();
   const router = useRouter();
+  const setUser = UserStore((state) => state.setName);
 
   useEffect(() => {
     const checkSession = async () => {
       const { data, error } = await supabase.auth.getSession();
       console.log('data', data);
+      setUser(data.session?.user?.user_metadata?.full_name);
+
       if (error || !data.session) {
         console.error("Authentication failed:", error);
         router.push("/login");
