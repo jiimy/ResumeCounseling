c4f57c6d65b9fc78e13c377c26cfc132418305d3
rudwnok <rudwnok123@naver.com>
2025-04-29 14:35:52 +0900

    리다이렉트 테스트

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,27 +12,27 @@ const Index = () => {
     const supabase = createClient();
     const redirectUrl = `${window.location.origin}/auth/callback`;
 
-    if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-      alert('배포환경입니다.');
-    }
-
-    // const { data, error } = await supabase.auth.signInWithOAuth({
-    //   provider: 'kakao',
-    //   options: {
-    //     redirectTo: process.env.NEXT_PUBLIC_VERCEL_URL
-    //       ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/callback`
-    //       : 'http://localhost:3000/auth/callback'
-    //     // redirectTo: `${window.location.origin}/auth/callback`
-    //     // redirectTo: location.origin + "/auth/callback",
-    //     // redirectTo: 'http://localhost:3000/auth/callback'
-    //   },
-    // });
-
-    // if (error) {
-    //   console.error("OAuth 로그인 에러:", error);
-    // } else {
-    //   console.log("로그인 성공:", data);
+    // if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    //   alert('배포환경입니다.');
     // }
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'kakao',
+      options: {
+        redirectTo: process.env.NEXT_PUBLIC_VERCEL_URL
+          ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/callback`
+          : 'http://localhost:3000/auth/callback'
+        // redirectTo: `${window.location.origin}/auth/callback`
+        // redirectTo: location.origin + "/auth/callback",
+        // redirectTo: 'http://localhost:3000/auth/callback'
+      },
+    });
+
+    if (error) {
+      console.error("OAuth 로그인 에러:", error);
+    } else {
+      console.log("로그인 성공:", data);
+    }
   };
 
   return (
