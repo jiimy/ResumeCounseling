671f8320c8103464717266fd8b1ded3026aa1b53
rudwnok <rudwnok123@naver.com>
2025-05-19 14:21:26 +0900

    test

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,21 +8,15 @@ import { createClient } from "@/util/supabase/client";
 const Index = () => {
   const router = useRouter();
 
-  const signInWithKakao = async () => {
+    const signInWithKakao = async () => {
     const supabase = createClient();
     const redirectUrl = `${window.location.origin}/auth/callback`;
-
-    // if (process.env.NEXT_PUBLIC_VERCEL_URL) {
-      // alert(process.env.NEXT_PUBLIC_VERCEL_URL);
-      // router.push(`${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/callback`);
-    // }
 
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'kakao',
       options: {
         redirectTo: process.env.NEXT_PUBLIC_VERCEL_URL
-          // ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/callback`
-          ? `${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/callback`
+          ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/auth/callback`
           : 'http://localhost:3000/auth/callback'
         // redirectTo: `${window.location.origin}/auth/callback`
         // redirectTo: location.origin + "/auth/callback",
@@ -31,8 +25,7 @@ const Index = () => {
     });
 
     if (error) {
-      // console.error("OAuth 로그인 에러:", error);
-      alert(error);
+      console.error("OAuth 로그인 에러:", error);
     } else {
       console.log("로그인 성공:", data);
     }
